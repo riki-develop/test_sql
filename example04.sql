7151fbc8aa45838cce85682de02fd117c90d6682
riki karasawa <fabulous.riki@gmail.com>
2021-09-09 08:48:06 +0900

    VIEWの削除「DROP VIEW」

--- example04.sql
+++ example04.sql
@@ -1,6 +1,6 @@
 /*
+■レクチャー104
 ▼ビューの作り方
-
 ・CREATE VIEW文を使う
 ↓
 CREATE VIEW ビュー名（<ビューの列名1>, <ビューの列名2>...）
@@ -30,6 +30,19 @@ ON u.prefecture_id = p.id
 GROUP BY u.prefecture_id;
 ↓
 -- 上記で生成したVIEWを呼び出す
+SELECT name, count
+FROM prefecture_user_counts;
+
+--------------------------------------
+/*
+■レクチャー106
+ビューの削除「DROP VIEW」
+*/
+
+-- 削除構文
+DROP VIEW prefecture_user_counts;
+
+-- VIEW存在確認
 SELECT name, count
 FROM prefecture_user_counts;
 
