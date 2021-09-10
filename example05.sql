759ec70a498d0f51ff2e13bf2816a457d2aa8180
riki karasawa <fabulous.riki@gmail.com>
2021-09-10 14:38:12 +0900

    演習：サブクエリ「WHERE句」

--- example05.sql
+++ example05.sql
@@ -31,3 +31,29 @@ WHERE order_time >= '2017-12-01 00:00:00'
 AND order_time < '2018-12-01 00:00:00');
 
 --------------------------------------
+/*
+■レクチャー109〜110
+演習：サブクエリ「WHERE句」
+
+▼課題
+・2017年12月に、商品を購入して"した"ユーザーにメルマガを打ちたい
+・該当ユーザー一覧を出力
+・必要な情報はユーザーID、名字、email
+
+▼復習：in 演算子
+・ある値が値セット内に含まれて"いる"かどうか
+・idが1か2か3の行を取得
+SELECT *
+FROM products
+WHERE id in(1, 2, 3);
+*/
+
+SELECT id, last_name, email
+FROM users
+WHERE id IN(
+SELECT user_id
+FROM orders
+WHERE order_time >= '2017-12-01 00:00:00'
+AND order_time < '2018-01-01 00:00:00');
+
+--------------------------------------
