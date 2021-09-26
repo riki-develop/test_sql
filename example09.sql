81766d5cd68b114a9da5b48373fe25855f356f44
riki karasawa <fabulous.riki@gmail.com>
2021-09-26 17:31:21 +0900

    新規DBに新規テーブルを作成

--- example09.sql
+++ example09.sql
@@ -33,3 +33,45 @@ Database
 'sys'
 
 --------------------------------------
+/*
+■レクチャー136
+テーブルの追加
+
+「books」というテーブルを作成
+↓
+▼例
+- id・・・行のID
+- title・・・本のタイトルを格納する
+*/
+
+-- 利用するDBを選択
+USE book_store;
+
+-- 現在のテーブル一覧
+SHOW TABLES;
+
+-- 空のDBにテーブルを作成
+CREATE TABLE books(id int not null auto_increment primary key,title varchar(255)not null);
+↓
+/*
+▼解説
+
+・列名（id）
+- int：整数型
+- not unll：nullを許可しない
+- auto_increment：idを自動的にふる
+- primary key：主キーの設定
+
+・列名（title）
+- varchar(255)：最大255文字の可変長文文字列型
+- not null：nullを許可しなし
+*/
+↓
+-- 作成したテーブルと中身（カラム）を確認
+SHOW COLUMNS FROM books;
+↓
+Field, Type, Null, Key, Default, Extra
+'id','int','NO','PRI',NULL,'auto_increment'
+'title','varchar(255)','NO','',NULL,''
+
+--------------------------------------
