824ff5fcc8db4c969b46ca7cf2df415781f6c1e9
riki karasawa <fabulous.riki@gmail.com>
2021-10-09 16:29:46 +0900

    テーブル構造の変更：列の追加、列の変更、列の削除

--- example09.sql
+++ example09.sql
@@ -75,3 +75,60 @@ Field, Type, Null, Key, Default, Extra
 'title','varchar(255)','NO','',NULL,''
 
 --------------------------------------
+/*
+■レクチャー137
+・テーブル構造の変更について
+- 列の追加
+- 列の変更
+- 列の削除
+*/
+
+-- 例題①
+-- 書籍を管理する'books'テーブルに、価格を管理する列を追加
+-- 列名：'price', データ型：'int'を追加
+↓
+-- 利用するDBを選択
+USE book_store;
+-- 現在のテーブル一覧
+SHOW TABLES;
+-- 'books'テーブルのカラム一覧を表示
+SHOW COLUMNS FROM books;
+↓
+-- カラム追加クエリ「ADD」
+ALTER TABLE books ADD price INT AFTER id;
+-- 'books'テーブルに'price'カラムが追加されているか確認
+SHOW COLUMNS FROM books;
+↓
+Field	Type	Null	Key	Default	Extra
+id	int	NO	PRI	NULL	auto_increment
+price	int	YES		NULL
+title	varchar(255)	NO		NULL
+
+-- 例題②
+-- 列名の変更
+-- 列名'price'を'unit_price'に変更
+
+-- 列名変更クエリ「CHANGE」
+ALTER TABLE books CHANGE price unit_price int;
+-- 指定した列名が変更されているか確認
+SHOW COLUMNS FROM books;
+↓
+Field, Type, Null, Key, Default, Extra
+'id','int','NO','PRI',NULL,'auto_increment'
+'unit_price','int','YES','',NULL,''
+'title','varchar(255)','NO','',NULL,''
+
+-- 例題③
+-- 列の削除
+-- 'books'テーブルから列'unit_price'を削除
+
+-- 列削除クエリ「DROP」
+ALTER TABLE books DROP unit_price;
+-- 指定した列が削除されているか確認
+SHOW COLUMNS FROM books;
+↓
+Field, Type, Null, Key, Default, Extra
+'id','int','NO','PRI',NULL,'auto_increment'
+'title','varchar(255)','NO','',NULL,''
+
+--------------------------------------
